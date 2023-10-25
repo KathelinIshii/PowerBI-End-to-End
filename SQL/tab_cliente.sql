0c1d4acbf93fc0e37e6be527683c592fcb880d18
Kathelin <kathelinishii@outlook.com>
2023-10-25 18:45:16 +0100

    create view

--- SQL/tab_cliente.sql
+++ SQL/tab_cliente.sql
@@ -10,3 +10,14 @@ ON PP.BusinessEntityID = SC.CustomerID
 ORDER BY NomeCustomer DESC;
 
 --Maiores clientes com base nas vendas (Nome do cliente, valor total da venda por dia, mês ou ano)
+
+CREATE VIEW SalesCustomer AS
+SELECT
+	PP.FirstName + ' ' + PP.LastName AS NomeCustomer,
+	SOH.OrderDate,
+	SOH.TotalDue
+FROM Sales.SalesOrderHeader AS SOH
+INNER JOIN Sales.Customer AS SC
+ON SC.CustomerID = SOH.CustomerID
+INNER JOIN Person.Person PP
+ON PP.BusinessEntityID = SC.CustomerID;
--- SQL/tab_produtos.sql
+++ SQL/tab_produtos.sql
@@ -9,3 +9,14 @@ INNER JOIN Sales.SalesOrderHeader SOH
 ON SOD.SalesOrderID = SOH.SalesOrderID;
 
 --Produtos mais vendidos com base no valor das vendas (Nome do produto, valor vendido por dia, mês ou ano).
+
+CREATE VIEW SalesProduct AS
+SELECT
+	PP.Name,
+	SOH.OrderDate,
+	SOD.LineTotal
+FROM Sales.SalesOrderDetail AS SOD
+INNER JOIN Production.Product AS PP
+ON SOD.ProductID = PP.ProductID
+INNER JOIN Sales.SalesOrderHeader SOH
+ON SOD.SalesOrderID = SOH.SalesOrderID;

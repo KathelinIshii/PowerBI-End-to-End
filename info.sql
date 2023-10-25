94ae8e5ecc16fe168763aae7611d36cf000dc71f
Kathelin <kathelinishii@outlook.com>
2023-10-25 17:05:27 +0100

    Update info.sql

--- info.sql
+++ info.sql
@@ -4,6 +4,8 @@ SELECT * FROM Sales.SalesOrderHeader;
 SELECT * FROM Sales.SalesOrderDetail;
 
 --Maiores clientes com base nas vendas (Nome do cliente, valor total da venda por dia, mês ou ano)
+
+
 --Produtos mais vendidos com base no valor das vendas (Nome do produto, valor vendido por dia, mês ou ano).
  
 SELECT * FROM Person.Person

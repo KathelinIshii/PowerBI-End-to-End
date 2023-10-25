924ec2609e7699d1e32526c630f401b65532ae75
Kathelin <kathelinishii@outlook.com>
2023-10-25 16:44:25 +0100

    Update tab_venda.sql

--- tab_venda.sql
+++ tab_venda.sql
@@ -1,14 +1,9 @@
-SELECT * FROM Sales.SalesOrderHeader;
---DATA, CUSTOMER ID
-
-SELECT * FROM Sales.SalesOrderDetail;
-
---Maiores clientes com base nas vendas (Nome do cliente, valor total da venda por dia, mês ou ano)
---Produtos mais vendidos com base no valor das vendas (Nome do produto, valor vendido por dia, mês ou ano).
- 
-SELECT * FROM Person.Person
---NOME DO CLIENTE 
-
-SELECT * FROM Sales.Customer
-
-SELECT * FROM Person.Person
+SELECT
+	PP.FirstName + ' ' + PP.LastName,
+	SOH.OrderDate,
+	SOH.TotalDue
+FROM Sales.SalesOrderHeader AS SOH
+INNER JOIN Sales.Customer AS SC
+ON SC.CustomerID = SOH.CustomerID
+INNER JOIN Person.Person PP
+ON PP.BusinessEntityID = SC.CustomerID;

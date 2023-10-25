a349366d42dc6cc8fc2f7afa1e40f774a2050620
Kathelin <kathelinishii@outlook.com>
2023-10-25 16:45:56 +0100

    Update tab_venda.sql

--- tab_venda.sql
+++ tab_venda.sql
@@ -7,3 +7,5 @@ INNER JOIN Sales.Customer AS SC
 ON SC.CustomerID = SOH.CustomerID
 INNER JOIN Person.Person PP
 ON PP.BusinessEntityID = SC.CustomerID;
+
+--Maiores clientes com base nas vendas (Nome do cliente, valor total da venda por dia, mês ou ano)

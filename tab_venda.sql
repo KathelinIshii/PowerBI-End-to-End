f0ba673300458b508cd9e863e6a72109a04459eb
Kathelin <kathelinishii@outlook.com>
2023-10-25 14:58:54 +0100

    Create tab_venda.sql

---
+++ tab_venda.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Sales.SalesOrderHeader;
+
+SELECT * FROM Sales.SalesOrderDetail;
+
+--Maiores clientes com base nas vendas (Nome do cliente, valor total da venda por dia, mês ou ano)
+   
